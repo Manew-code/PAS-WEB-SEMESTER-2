9a3da53fda2d3ba1c1ce192612f7bcd75963c6f9
kinyol-code <rifkypras007@gmail.com>
2024-03-14 23:31:06 +0700

    PTS WEB

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})-> name('home');
+Route::get('/smktelkompwt', function () {
+    return view('smktelkompwt');
+})-> name('masuk');
+
+Route::get('/dashboard', [DashboardController::class,'index']);
+Route::get('/pelanggan', [PelangganController::class,'index']);
